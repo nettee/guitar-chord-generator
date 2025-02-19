2ebcd2d8232bf2198b2634a85902af4032454880
nettee <nettee.liu@gmail.com>
2025-02-19 22:18:33 +0800

    main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+import { ChordBox } from './dist/bundle.js';
+
+let chordBox = new ChordBox('#chord1', {
+  // showTuning: false,
+});
+
+chordBox.draw({
+  chord: [[1, 2], [2, 1], [3, 2], [4, 0], [5, 'x'], [6, 'x']],
+  // optional: position marker
+  position: 5, // start render at fret 5
+
+  // positionText: 4,
+
+  // optional: barres for barre chords
+  barres: [
+    { fromString: 6, toString: 1, fret: 1 },
+    { fromString: 5, toString: 3, fret: 3 }
+  ],
+
+  // optional: tuning keys
+  tuning: ['E', 'A', '2', 'G', 'B', 'E'],
+});
