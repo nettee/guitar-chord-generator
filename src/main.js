57b58a624bcb62677fece9503842c9d36be97fb1
William Liu <nettee.liu@gmail.com>
2025-02-22 08:42:18 +0000

    F chord

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ let chordBox = new ChordBox('#chord1', {
 });
 
 chordBox.draw({
-  chord: [[1, 2], [2, 1], [3, 2], [4, 0], [5, 'x'], [6, 'x']],
+  chord: [[1, 1], [2, 1], [3, 2], [4, 3], [5, 3], [6, 1]],
   // optional: position marker
-  position: 5, // start render at fret 5
+  // position: 5, // start render at fret 5
 
   // positionText: 4,
 
   // optional: barres for barre chords
   barres: [
     { fromString: 6, toString: 1, fret: 1 },
-    { fromString: 5, toString: 3, fret: 3 }
+    // { fromString: 5, toString: 3, fret: 3 }
   ],
 
   // optional: tuning keys
-  tuning: ['E', 'A', '6', 'G', 'B', 'E'],
+  tuning: ['R', '5', 'R', 'T', '5', 'R'],
 });
