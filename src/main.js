b9009116ed1ea83573784b4138c706f7db77c1b5
nettee <nettee.liu@gmail.com>
2025-03-02 17:17:24 +0800

    chord group

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import {ChordBox} from './chordbox.js';
 
+function drawChord(id, chord) {
+  const sel = `#${id}`;
+  new ChordBox(sel, {
+    numFrets: 4,
+  }).draw(chord);
+}
+
 let F = {
   chord: [[1, 1], [2, 1], [3, 2], [4, 3], [5, 3], [6, 1]],
   barres: [
@@ -8,16 +15,44 @@ let F = {
   tuning: ['R', '5', 'R', 'T', '5', 'R'],
 };
 
-new ChordBox('#chord1', {
-  // showTuning: false,
-}).draw(F);
-
 let G = {
   chord: [[1, 3], [2, 3], [3, 0], [4, 0], [5, 2], [6, 3]],
   tuning: ['R', 'T', '5', 'R', '5', 'R'],
 };
 
-new ChordBox('#chord2', {
-  // showTuning: false,
-}).draw(G);
+let Em = {
+  chord: [[1, 0], [2, 0], [3, 0], [4, 2], [5, 2], [6, 0]],
+  tuning: ['R', '5', 'R', 'T', '5', 'R'],
+};
+
+let Am = {
+  chord: [[1, 0], [2, 1], [3, 2], [4, 2], [5, 0], [6, 0]],
+  tuning: ['R', '5', 'R', 'T', '5', 'R'],
+};
+
+let Dm = {
+  chord: [[1, 1], [2, 3], [3, 2], [4, 0], [5, 0], [6, 0]],
+  tuning: ['R', '5', 'R', 'T', '5', 'R'],
+};
+
+let C = {
+  chord: [[1, 0], [2, 1], [3, 0], [4, 2], [5, 3], [6, 0]],
+  tuning: ['R', '5', 'R', 'T', '5', 'R'],
+};
+
+const groupName = 'C-4536251';
+
+const chordElements = document.querySelectorAll('.chord-group .chord');
+const ids = [];
+for (let i = 0; i < chordElements.length; i++) {
+  const id = `group-${groupName}-chord-${i + 1}`;
+  chordElements[i].setAttribute('id', id);
+  ids.push(id);
+}
+
+const chords = [F, G, Em, Am, Dm, G, C];
+
+for (let i = 0; i < chords.length; i++) {
+  drawChord(ids[i], chords[i]);
+}
 
