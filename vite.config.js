a3357f2995952fc14e94943e2e900d1b4a3c158d
nettee <nettee.liu@gmail.com>
2025-03-25 21:05:04 +0800

    migrate to sass

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 如果需要添加全局变量或mixins
+        // additionalData: `@import "./src/scss/_variables.scss";`
+      }
+    }
+  }
 });
